3b78fb9de2bcefe32075ae899a81dc87cc71633f
Paolo Brandoli <paolo.brandoli@gmail.com>
2019-08-24 18:46:53 +0200

    Addresses #297. Updated getVersion.py and uploadDropbox.py to work with Python3

--- uploadDropbox.py
+++ uploadDropbox.py
@@ -10,7 +10,7 @@ import subprocess
 #----------------
 hgbranch = 'hg branch'
 hgprocess = subprocess.Popen(hgbranch.split(), stdout=subprocess.PIPE)
-branch = hgprocess.stdout.readlines()[0].rstrip()
+branch = hgprocess.stdout.readlines()[0].decode("utf-8").rstrip()
 
 version = branch
 if version == "default":
@@ -23,7 +23,7 @@ if version.count('.') == 1:
 
 hgrelease = 'hg log -b ' + branch + ' --template . --rev ancestors(.)'
 hgprocess = subprocess.Popen(hgrelease.split(), stdout=subprocess.PIPE)
-release = version + '.' + str(hgprocess.stdout.readlines()[0].count('.'))
+release = version + '.' + str(hgprocess.stdout.readlines()[0].decode("utf-8").count('.'))
 
 file_name = "/imebra_" + release.replace('.', '_') + '.zip'
 
